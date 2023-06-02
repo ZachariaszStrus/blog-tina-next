1684651aff78958755856fee7aadf3ff4bfe017a
Zachariasz Strus <zachariasz.strus@gmail.com>
2023-06-02 13:30:54 +0200

    init commit

---
+++ tina/__generated__/client.ts
@@ -0,0 +1,5 @@
+import { createClient } from "tinacms/dist/client";
+import { queries } from "./types";
+export const client = createClient({ url: 'https://content.tinajs.io/1.4/content/6054d6e9-a640-40a5-883f-ba0a2db44867/github/main', token: '5cb965925abd9043c51d5a7b1aa2885cc66357d1', queries });
+export default client;
+  
---
+++ tina/config.ts
@@ -0,0 +1,49 @@
+import { defineConfig } from "tinacms";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "aga";
+
+export default defineConfig({
+  branch: "aga",
+  clientId: "6054d6e9-a640-40a5-883f-ba0a2db44867",
+  token: "5f01bd695c8d54a96ad38053fe6e8ffbbfabf70b",
+
+  build: {
+    outputFolder: "admin",
+    publicFolder: "public",
+  },
+  media: {
+    tina: {
+      mediaRoot: "",
+      publicFolder: "public",
+    },
+  },
+  schema: {
+    collections: [
+      {
+        name: "post",
+        label: "Posts",
+        path: "content/posts",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
+        ui: {
+          // This is an DEMO router. You can remove this to fit your site
+          router: ({ document }) => `/demo/blog/${document._sys.filename}`,
+        },
+      },
+    ],
+  },
+});
